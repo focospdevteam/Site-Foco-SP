ce591713d24a754b9bca7eb249544d9907d67c2e
Renato Martins <almeidarenatosantista@gmail.com>
2020-12-30 02:51:44 -0300

    alteracao no style

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function ativarMenuMobile(){
 }
 
 function navegarMenuMobile(){
-    const li = document.querySelectorAll("ul li a");
+    const li = document.querySelectorAll(".nav-menu ul li a");
     li.forEach((item)=>{
         item.addEventListener("click",ativarMenuMobile)
         item.addEventListener("click",(event)=>{
@@ -12,6 +12,24 @@ function navegarMenuMobile(){
                 item.classList.remove("active")
             })
             event.currentTarget.classList.toggle("active");
+        })
+    })
+}
+
+function ativarflip(){
+    const buttons = document.querySelectorAll(".card a");
+    const cards = document.querySelectorAll(".card .flip")
+    let last = 0
+    buttons.forEach((item,index)=>{
+        item.addEventListener("click",(evt)=>{
+            evt.preventDefault()
+            let flip_card = item.offsetParent.offsetParent
+            if(flip_card.style.transform == "rotateY(180deg)"){
+                flip_card.style.transform =  "rotateY(360deg)";
+            }else{
+                flip_card.style.transform =  "rotateY(180deg)";
+            }            
+            // cards[index].classList.toggle("active")
         })
     })
 }
